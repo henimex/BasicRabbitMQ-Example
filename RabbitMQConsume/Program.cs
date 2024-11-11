b2c57281bbeb6601121d652b4d47f2d68b5ea7d4
henimex-ISM <henimex@gmail.com>
2024-11-11 16:20:08 +0300

    Add project files.

---
+++ RabbitMQConsume/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQConsume
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string QueName = "Henimex";
+            string RabbitServer = "";
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(RabbitServer);
+
+            using var connection = factory.CreateConnection();
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: QueName,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                    );
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                channel.BasicConsume(
+                    queue: QueName,
+                    autoAck: true,
+                    consumer: consumer);
+
+
+                consumer.Received += Consumer_Received;
+
+                Console.WriteLine("Enter To Exit");
+                Console.ReadLine();
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [x] Received {0}", message);
+        }
+    }
+}
---
+++ RabbitMQSend/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Text;
+
+namespace RabbitMQSend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string QueName = "Henimex";
+            string RabbitServer = "";
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(RabbitServer);
+
+            using var connection = factory.CreateConnection();
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: QueName,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                    );
+
+                string message = "Hello World of RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: QueName,
+                    basicProperties: null,
+                    body: body
+                    );
+
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine("Waitng for exit");
+            Console.ReadLine();
+        }
+    }
+}
